02942e0e3252e6bff6abb30f7067ccc7fe752d2a
杨恺 <yangk@ersoft.cn>
2016-12-11 11:54:49 +0800

    再次上传,删除.idea/文件夹

--- ch04/prodcons.py
+++ ch04/prodcons.py
@@ -11,35 +11,37 @@ from threading import Lock
 lock = Lock()
 
 def writeQ(queue):
-    print 'producing object for Q...',
     queue.put('xxx', 1)
-    print "size now", queue.qsize()
+    with lock:
+        print 'producing object for Q...',
+        print "size now", queue.qsize()
 
 def readQ(queue):
     val = queue.get(1)
-    print 'consumed object from Q... size now', queue.qsize()
+    with lock:
+        print 'consumed object from Q... size now', queue.qsize()
+        print val
 
 def writer(queue, loops):
     for i in range(loops):
         writeQ(queue)
-        sleep(randrange(1, 4))
+        sleep(randrange(2, 6))
 
 def reader(queue, loops):
     for i in range(loops):
         readQ(queue)
-        sleep(randrange(2, 6))
 
 funcs = [writer, reader]
 nfuncs = range(len(funcs))
 
 def main():
-    nloops = randrange(2, 6)
+    nloops = randrange(2, 20)
     q = Queue(32)
 
     threads = []
     for i in nfuncs:
         t = MyThread(funcs[i], (q, nloops), \
-            funcs[i].__name__, True)
+            funcs[i].__name__)
         threads.append(t)
 
     for i in nfuncs:
